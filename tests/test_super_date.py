a4fb6f1af727c56db19323e08bbc7d19f8f148a8
Logan Reece <onereddime@protonmail.com>
2023-03-18 14:28:34 -0500

    More robust unit tests
    Also they're failing because dateutil and paresdatetime
    are handling different days for times
    
    dateutil handles "wednesday" as 'today', but parsedatetime,
    when given a time, seems to handle it as next wednesday. Lame

--- tests/test_super_date.py
+++ tests/test_super_date.py
@@ -1,8 +1,8 @@
 import unittest
 from datetime import date, datetime
 
-from superdate.parse_date import parse_date
-from superdate.SuperDate import SuperDate
+from superdate import parse_date
+from superdate import SuperDate as SD
 
 
 class TestParseDate(unittest.TestCase):
@@ -10,73 +10,51 @@ class TestParseDate(unittest.TestCase):
     def test_basic_date_parsing(self):
         """ Make sure the underlying data is a date.
         """
-        act = SuperDate('today')
+        act = SD('today')
         exp = parse_date('today')
         self.assertEqual(exp, act._date)
 
     def test_is_date(self):
         """ Something with no time elements should result in 'date' type
         """
-        act = SuperDate('today')
+        act = SD('today')
         self.assertTrue(type(act._date) is date)
 
     def test_is_datetime(self):
         """ Something with time elements should result in 'datetime' type
         """
-        act = SuperDate('now')
+        act = SD('now')
         self.assertTrue(type(act._date) is datetime)
 
     def test_str_equivalence(self):
         """ Output as str should equal actual date.
         """
-        exp = SuperDate('today')
-        act = SuperDate(str(exp))
+        exp = SD('today')
+        act = SD(str(exp))
         self.assertEqual(exp, act)
 
-    def test_comparison(self):
+    def test_super_date_comparison(self):
         """ Test comparison between two SuperDates
         """
-        d1 = SuperDate('today')
-        d2 = SuperDate('today')
+        d1 = SD('today')
+        d2 = SD('today')
         self.assertEqual(d1, d2)
         self.assertEqual(d1._date, d2._date)
 
-    def test_comparison_with_time(self):
-        """ Test comparison between two SuperDates, but one has a time.
+    def test_string_comparisons(self):
+        """ Test a whole bunch of comparisons.
         """
-        d1 = SuperDate('today')
-        d2 = SuperDate('now')
-        self.assertEqual(d1, d2)
+        # LT
+        self.assertEqual(SD('today'), 'now')
 
-    def test_lt(self):
-        """ Test < operator.
-        """
-        l_ = SuperDate('yesterday')
-        r = SuperDate('today')
-        self.assertTrue(l_ < r)
+        self.assertTrue(SD('yesterday') < 'today')
+        self.assertTrue(SD('Wednesday at noon') < 'Wednesday at 4pm')
 
-        l_ = SuperDate('Wednesday')
-        r = SuperDate('Wednesday at noon')
-        self.assertFalse(l_ < r)
+        self.assertFalse(SD('Wednesday') < 'Wednesday at noon')
+        self.assertFalse(SD('Wednesday at noon') < 'Wednesday')
 
-        l_ = SuperDate('Wednesday at noon')
-        r = SuperDate('Wednesday at 4pm')
-        self.assertTrue(l_ < r)
-
-        l_ = SuperDate('Wednesday at noon')
-        r = SuperDate('Wednesday')
-        self.assertFalse(l_ < r)
-
-    def test_gt(self):
-        """ Test > operator.
-        """
-        l_ = SuperDate('yesterday')
-        r = SuperDate('today')
-        self.assertTrue(r > l_)
-
-        # with time
-        r = SuperDate('now')
-        self.assertTrue(r > l_)
+        # Gt
+        self.assertTrue(SD('today') > 'yesterday')
 
 
 if __name__ == '__main__':
